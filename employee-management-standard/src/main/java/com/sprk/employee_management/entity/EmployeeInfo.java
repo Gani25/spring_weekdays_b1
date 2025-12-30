7385c6548ba725eac5d3a55b7a6f92bb739cd771
Gani25 <memonabdulgani3d@gmail.com>
2025-12-30 12:26:38 +0530

    Copy Paste Maven Project and CHange name

---
+++ employee-management-standard/src/main/java/com/sprk/employee_management/entity/EmployeeInfo.java
@@ -0,0 +1,23 @@
+package com.sprk.employee_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity(name = "employee")
+//@Table(name = "employee_sprk")
+public class EmployeeInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empId;
+
+    @Column(name = "emp_name", nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+//    @Column(columnDefinition = " enum('Male', 'Female', 'Other')")
+    private String gender;
+}
